89d4d17d9f610ddd49400f5b642218aa9404dfb2
ar0177417 <ar0177417@gmail.com>
2023-12-16 19:10:41 +0500

    fix device.mk

--- device.mk
+++ device.mk
@@ -16,17 +16,20 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.mt6768 \
+    bootctrl.mt6768.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6768
+    bootctrl
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6768 \
-    libgptutils \
-    libz \
-    libcutils
-
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#     bootctrl.mt6768 \
+#     libgptutils \
+#     libz \
+#     libcutils
+#
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
