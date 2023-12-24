c34ad3cf2c7f201c025aaeb1b303e0095c86db04
Ahmad Rehan <67918673+ardrag0n@users.noreply.github.com>
2023-12-24 16:51:55 +0500

    Add crypto Back

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,19 +144,19 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 
 
 # Crypto
-#TW_INCLUDE_CRYPTO := true
-#TW_INCLUDE_CRYPTO_FBE := true
-#BOARD_USES_METADATA_PARTITION := true
-#TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+BOARD_USES_METADATA_PARTITION := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
 
 # Recovery modules
-#TARGET_RECOVERY_DEVICE_MODULES += \
-#    libkeymaster4 \
-#    libpuresoftkeymasterdevice
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
 
-#RECOVERY_LIBRARY_SOURCE_FILES += \
-#    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
- #   $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
